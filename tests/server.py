d79c0effa0cb33a2f736fc1491a802f235de0a52
Amber Brown <hawkowl@atleastfornow.net>
2018-06-11 23:25:49 +1000

    some more testing harnesses

--- tests/server.py
+++ tests/server.py
@@ -1,19 +1,33 @@
 from io import BytesIO
 
 import attr
+import json
 from six import text_type
+
+from twisted.internet import reactor
+from twisted.python.failure import Failure
+from twisted.internet.defer import Deferred
+from twisted.test.proto_helpers import MemoryReactorClock
 
 from mock import Mock
 from synapse.api.ratelimiting import Ratelimiter
 from synapse.http.site import SynapseRequest
 from synapse.server import HomeServer
 from synapse.util import Clock
+from twisted.internet import threads
+from tests.utils import setup_test_homeserver as _sth
 
 
 @attr.s
 class FakeChannel(object):
 
     result = attr.ib(factory=dict)
+
+    @property
+    def json_body(self):
+        if not self.result:
+            raise Exception("No result yet.")
+        return json.loads(self.result["body"])
 
     def writeHeaders(self, version, code, reason, headers):
         self.result["version"] = version
@@ -108,6 +122,60 @@ def wait_until_result(clock, channel, timeout=100):
 
     while not channel.result:
         x += 1
+
         if x > timeout:
             raise Exception("Timed out waiting for request to finish.")
+
         clock.advance(0.1)
+
+
+class ThreadedMemoryReactorClock(MemoryReactorClock):
+
+    def callFromThread(self, callback, *args, **kwargs):
+        d = Deferred()
+        d.addCallback(lambda x: callback(*args, **kwargs))
+        self.callLater(0, d.callback, True)
+        return d
+
+
+def setup_test_homeserver(*args, **kwargs):
+
+    d = _sth(*args, **kwargs).result
+
+    # Make the pool synchronous.
+    clock = d.get_clock()
+    pool = d.get_db_pool()
+
+    def runWithConnection(func, *args, **kwargs):
+        return threads.deferToThreadPool(pool._reactor, pool.threadpool,
+                                         pool._runWithConnection,
+                                         func, *args, **kwargs)
+    def runInteraction(Interaction, *args, **kwargs):
+        return threads.deferToThreadPool(pool._reactor, pool.threadpool,
+                                         pool._runInteraction,
+                                         interaction, *args, **kw)
+
+    pool.runWithConnection = runWithConnection
+    pool.runInteraction = runInteraction
+
+    class ThreadPool():
+
+        def start(self):
+            pass
+
+        def callInThreadWithCallback(self, onResult, function, *args, **kwargs):
+
+            def _(res):
+                if isinstance(res, Failure):
+                    onResult(False, res)
+                else:
+                    onResult(True, res)
+
+            d = Deferred()
+            d.addCallback(lambda x: function(*args, **kwargs))
+            d.addBoth(_)
+            clock._reactor.callLater(0, d.callback, True)
+            return d
+
+    pool.threadpool = ThreadPool()
+    return d
